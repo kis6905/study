a0d824f5bce02ac0938ea4f9c4f056467ad9077a
kis6905 <kis6905@gmail.com>
2021-09-18 17:41:52 +0900

    web-basis 추가

---
+++ web-basis/src/leaf/web/basis/SimpleHttpServer.java
@@ -0,0 +1,58 @@
+package leaf.web.basis;
+
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleHttpServer {
+
+	private static final int BUFFER_SIZE = 4096;
+	
+	public static void main(String[] args) throws Exception {
+		
+		ServerSocket serverSocket = new ServerSocket(8080);
+		System.out.println("Start http server...\n");
+		while (true) {
+			Socket socket = serverSocket.accept();
+			
+			InputStream input = socket.getInputStream();
+			OutputStream output = socket.getOutputStream();
+			
+			byte[] buffer = new byte[BUFFER_SIZE];
+			StringBuilder requestBuffer = new StringBuilder();
+			
+			int i = 0;
+			while (true) {
+				buffer = new byte[BUFFER_SIZE];
+	            i = input.read(buffer);
+		        for (int j = 0; j < i; j++) {
+		            requestBuffer.append((char) buffer[j]);
+		        }
+		        
+		        if (input.available() <= 0) {
+		        	break;
+		        }
+			}
+			
+			System.out.println("request: ");
+			System.out.println(requestBuffer.toString());
+			
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(output));
+			bw.write("HTTP/1.1 200 OK\n");
+			bw.write("content-type: text/html;\n");
+			bw.write("\n");
+			bw.write("<h1>hello world</h1>");
+			
+			bw.flush();
+			input.close();
+			bw.close();
+			socket.close();
+			
+			System.out.println("\n\n");
+		}
+	}
+
+}
