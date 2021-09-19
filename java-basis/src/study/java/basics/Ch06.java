e519c5527fbe93ed0886db728a5828deb2eb243b
iskwon <kis6905@gmail.com>
2021-09-19 16:38:54 +0900

    ch06 추가

---
+++ java-basis/src/study/java/basics/Ch06.java
@@ -0,0 +1,18 @@
+package study.java.basics;
+
+/**
+ * Chapter06 - 객체지향 프로그래밍
+ * - 상속
+ * - 오버라이딩
+ * - package와 import
+ * - 다형성
+ * - 추상클래스
+ * - 인터페이스
+ */
+public class Ch06 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
