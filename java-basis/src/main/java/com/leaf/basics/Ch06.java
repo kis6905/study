7909fc60d463fd096254a8df49e5b926961d2050
kis6905 <kis6905@gmail.com>
2022-05-16 23:09:31 +0900

    내용 추가

--- java-basis/src/main/java/com/leaf/basics/Ch06.java
+++ java-basis/src/main/java/com/leaf/basics/Ch06.java
@@ -3,8 +3,14 @@ package com.leaf.basics;
 /**
  * Chapter06 - 객체지향 프로그래밍
  * - 상속
+ *   - 장점
+ *   - 단일 상속
+ *   - Object 클래스
  * - 오버라이딩
+ * - super
  * - package와 import
+ * - 접근제어자
+ *   - 캡슐화
  * - 다형성
  * - 추상클래스
  * - 인터페이스
@@ -12,7 +18,7 @@ package com.leaf.basics;
 public class Ch06 {
 
 	public static void main(String[] args) {
-		
+
 	}
 
 }
