751878a0b72f9b79164de29bce5d5d5eacd72fd0
kis6905 <kis6905@gmail.com>
2022-06-02 22:45:40 +0900

    ch 추가

--- java-basis/src/main/java/com/leaf/basics/Ch11.java
+++ java-basis/src/main/java/com/leaf/basics/Ch11.java
@@ -2,14 +2,14 @@ package com.leaf.basics;
 
 /**
  * Chapter11 - 쓰레드
- * - 임계영역
- * - 세마포어, 뮤텍스
  * - Thread, Runnable
+ * - 임계영역
+ * - 동기화
  * - ExecutorService
  */
 public class Ch11 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 	}
 
